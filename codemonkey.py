7286bd745e0290ba4b3e5a7e6d218e479ab5e56f
AlyssaR <arahman@smu.edu>
2015-12-23 10:03:40 -0600

    Added an uber basic config

--- codemonkey.py
+++ codemonkey.py
@@ -1,5 +1,31 @@
-def main():
-    print "This program's current capabilities are roughly equal to Russell's level of skill."
+import ConfigParser
 
+configs = {"install": [], "backup": [], "setup": [], "restore": []}
+
+def backup(stuff):
+    print "backup"
+
+def clean(stuff):
+    print "clean"
+
+def install(stuff):
+    print "install"
+
+def load_configs():
+    config = ConfigParser.ConfigParser()
+    config.read("config")
+    configs["backup"] = config.get("dirs", "backup").split(",")
+    configs["install"] = config.get("dirs", "install").split(",")
+    configs["restore"] = config.get("dirs", "restore").split(",")
+    configs["setup"] = config.get("dirs", "setup").split(",")
+    
+def restore(stuff):
+    print "restore"
+
+def setup(stuff):
+    print "setup"
+
+def main():
+    load_configs()
 
 if __name__ == '__main__': main()
