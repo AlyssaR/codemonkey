816fe2a5b6050f26587c9ab8e028ced5018b231c
AlyssaR <arahman@smu.edu>
2015-12-29 21:53:36 -0600

    Wrote skeleton for elk setup and am moving on because i have a short attention span

--- install/elk_i.py
+++ install/elk_i.py
@@ -100,5 +100,4 @@ def main():
     else:
         print "[!] Error", result
 
-
 if __name__ == '__main__': main()
--- setup/elk_s.py
+++ setup/elk_s.py
@@ -1,3 +1,5 @@
+import getpass
+
 def run(args):
     """
     INPUT:  args        List of arguments, may be a list of objects
@@ -8,11 +10,25 @@ def run(args):
     if check[0] == 'n' or check[0] == 'N':
         return "Please log out/in and run again."
 
+    """Create User for Docker"""
+
     """Start Docker-Engine and Compose DockerFile"""
     subprocess.Popen(["service", "docker", "start"], stdin=subprocess.PIPE, stdout=subprocess.PIPE).wait()
     os.system("cd resources/elk")
-    subprocess.Popen(["docker-compose", "up", "-d"], stdin=subprocess.PIPE, stdout=subprocess.PIPE).wait() #Run
+    subprocess.Popen(["docker-compose", "up", "-d"], stdin=subprocess.PIPE, stdout=subprocess.PIPE).wait()
     os.system("cd ../..")
     print "[+] Composed appliances"
 
+    """Set to Start on Boot"""
+
+    """Change Default Passwords"""
+
+    """Change Default Ports?"""
+
     return "Module not completed yet."
+
+def main():
+    print "Testing system"
+    print getpass.getuser()
+
+if __name__ == '__main__': main()
