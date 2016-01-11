ba5af248fcd7990ac589df3f58abca7a1bc4fb17
AlyssaR <arahman@smu.edu>
2016-01-11 08:53:00 -0600

    Testing

--- example/module.py
+++ example/module.py
@@ -1,14 +1,13 @@
 def run(args):
     """
     INPUT:  args        List of arguments, may be a list of objects
-    OUTPUT:             If success, just use a plain return 
+    OUTPUT:             If success, just use a plain return
             string      If error, return string error message
     """
-    print "Received input of:",
-    for a in args:
-        print ">", a
+    print ", ".join(args)
 
-    if len(args) < 2:
-        return "Not enough arguments"
-    else:
-        return
+
+def main():
+    run(["blah", "blah"])
+
+if __name__ == '__main__': main()
